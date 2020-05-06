fe7db7ffbbdd0d095f9f8451fd37c250772a6e10
Thordur Hermannsson <doddi@kott.is>
2020-05-06 23:10:33 +0000

    update version

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@
 }:
 mkDerivation {
   pname = "donnabot-service";
-  version = "0.0.12.1";
+  version = "0.0.12.2";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
--- shell.nix
+++ shell.nix
@@ -16,7 +16,7 @@ let
       }:
       mkDerivation {
         pname = "donnabot-service";
-        version = "0.0.12.1";
+        version = "0.0.12.2";
         src = ./.;
         isLibrary = true;
         isExecutable = true;
