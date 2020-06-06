c092604c6243393bd215647e764a973dd5fed12d
Donna <doddi@kott.is>
2020-06-06 19:05:41 +0000

    remove deriving-aeson reference

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@
 }:
 mkDerivation {
   pname = "donnabot-service";
-  version = "0.0.13.0";
+  version = "0.0.13.1";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
--- shell.nix
+++ shell.nix
@@ -16,7 +16,7 @@ let
       }:
       mkDerivation {
         pname = "donnabot-service";
-        version = "0.0.13.0";
+        version = "0.0.13.1";
         src = ./.;
         isLibrary = true;
         isExecutable = true;
